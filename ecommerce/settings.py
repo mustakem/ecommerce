973b75b191d7ebc7ce7f248783d5fb24b60f6f02
mustakem <mustakeem.world@gmail.com>
2022-08-29 12:25:51 +0600

    Data Structure and Models

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -112,7 +112,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
+MEDIA_URL = '/images/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
